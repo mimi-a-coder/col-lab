d80f70e2b7c4da5806aaf8f5d9b901ddd9238a9f
Leshan <leshan0627@gmail.com>
2024-03-10 23:18:44 -0400

    style: Add Style to Registration page

--- src/Components/Registration.jsx
+++ src/Components/Registration.jsx
@@ -10,12 +10,12 @@ export default function Registration() {
             </div>
         </div>
         <form className="form-registration">
-            <div className="row mb-3">
+            <div className="row">
                 <div className="col">
                     <p className="login-lead"><strong>Tell us a bit more about yourself...</strong></p>
                 </div>                
             </div>
-            <div className="row mb-4">
+            <div className="row">
                 <div className="col-lg-6">
                     <input className="form-control form-control-lg" type="text" placeholder="First Name" aria-label="First Name" required />
                 </div>                
@@ -23,21 +23,21 @@ export default function Registration() {
                     <input className="form-control form-control-lg" type="text" placeholder="Last Name" aria-label="First Name" required />
                 </div>                
             </div>
-            <div className="row mb-4">
+            <div className="row">
                 <div className="col-lg-6">
+                    <p className="small form-registration-label">Enter Your Birth Date</p>
                     <input className="form-control form-control-lg" type="Date" placeholder="Birthday" aria-label="Birthday" required />
-                    <p className="small">Enter Your Birth Date</p>
                 </div>                
                 <div className="col-lg-6">
                     <select className='form-control form-select' aria-label='Gender Selection'>
-                        <option selected disabled>Choose an Option</option>
+                        <option selected disabled>Choose A Gender</option>
                         <option>Man</option>
                         <option>Woman</option>
                         <option>Prefer Not To Say</option>
                     </select>
                 </div>                
             </div>
-            <div className="row mb-4">              
+            <div className="row">              
                 <div className="col-lg-6">
                     <input className="form-control form-control-lg" type="text" placeholder="Job Title" aria-label="Job Title" required />
                 </div>                
@@ -45,7 +45,7 @@ export default function Registration() {
                     <input className="form-control form-control-lg" type="text" placeholder="Institution" aria-label="Institution" required />
                 </div>                
             </div>
-            <div className="row mb-4">
+            <div className="row">
                 <div className="col-lg-6">
                     <input className="form-control form-control-lg" type="text" placeholder="Country of Residence" aria-label="Country" required />
                 </div>                
@@ -53,32 +53,32 @@ export default function Registration() {
                     <input className="form-control form-control-lg" type="text" placeholder="City" aria-label="City" required />
                 </div>                
             </div>
-            <div className="row mb-4">              
+            <div className="row">              
                 <div className="col">
                     <input className="form-control form-control-lg" type="text" placeholder="Field of Research" aria-label="Field of Research" required />
                 </div>                
             </div>
-            <div className="row mb-4">              
+            <div className="row">              
                 <div className="col">
                     <input className="form-control form-control-lg" type="text" placeholder="Current Degree Level" aria-label="Current degree level" required />
                 </div>                
             </div>
-            <div className="row mb-4">              
+            <div className="row">              
                 <div className="col">
                     <input className="form-control form-control-lg" type="text" placeholder="Indicate up to 20 skills/areas of expertise" aria-label="Indicate up to 20 skills/areas of expertise" required />
                 </div>                
             </div>
-            <div className="row mb-4">              
+            <div className="row">              
                 <div className="col">
                     <input className="form-control form-control-lg" type="Email" placeholder="Email" aria-label="Email" required />
                 </div>                
             </div>
-            <div className="row mb-4">              
+            <div className="row">              
                 <div className="col">
                     <input className="form-control form-control-lg" type="password" placeholder="Password" aria-label="password" required />
                 </div>                
             </div>
-            <div className="row mt-2 mb-4">
+            <div className="row mt-2">
                     <div className="col"> 
                         <button className="btn btn-lg btn-primary login-btn">Sign Up</button>
                     </div>                    
