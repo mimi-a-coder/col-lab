6b80db3303117b470b6d5e8323bc5b43be448e2c
Leshan <leshan0627@gmail.com>
2024-03-20 19:04:56 -0400

    style: Add navigation styles

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Brand from '../Images/colLAB-logo.svg'
 
 export default function Navigation() {
+ 
+  const logout = () => {
+    localStorage.removeItem('jwt');
+    localStorage.removeItem('user_name');
+    window.location.replace('/');
+  }
+
     return (
         <header>
             <div className="container-fluid primary">
@@ -9,37 +16,23 @@ export default function Navigation() {
                     <div className="col">
                         <nav className="navbar navbar-expand-lg">
                             <a className="nav-brand" href="#"><img className="brand" src={Brand}/><h1><i>colLabb</i></h1></a>
-                            <nav class="navbar navbar-expand-lg navbar-light">
-  <div class="container-fluid">
-
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
+                            <ul class="nav nav-pills">
+                              <li class="nav-item">
+                                <a class="nav-link active" href="#">Home</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="#">About Us</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="#">Contact Us</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="#">Your Profile</a>
+                              </li>
+                              <li class="nav-item">
+                                <a class="nav-link" href="#" onClick={logout}>Logout</a>
+                              </li>
+                            </ul>
                         </nav>
                     </div>
                 </div>
