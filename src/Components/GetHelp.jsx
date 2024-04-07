c5b9d2df312e31540356b2500e8e9c8a7c2c16aa
Leshan <leshan0627@gmail.com>
2024-04-07 03:57:26 -0400

    feat: Add search to get help page

--- src/Components/GetHelp.jsx
+++ src/Components/GetHelp.jsx
@@ -5,9 +5,10 @@ import axios from 'axios';
 export default function GetHelp() {
     const [ question, setQuestion ] = useState([]);
     const [ users, setUsers ] = useState([]);
+    const [ search, setSearch ] = useState('');
 
     useEffect(() => {
-        axios.get('https://pattersonselectric.com/wp-json/wp/v2/questions')
+        axios.get(`${process.env.REACT_APP_API_URL}/wp-json/wp/v2/questions`)
         .then((response) =>{
             setQuestion(response.data)
         }).catch((err) =>{
@@ -16,7 +17,7 @@ export default function GetHelp() {
     }, [])
 
     useEffect(() => {
-        axios.get('https://pattersonselectric.com/wp-json/wp/v2/users')
+        axios.get(`${process.env.REACT_APP_API_URL}/wp-json/wp/v2/users`)
         .then((response) => {
           setUsers(response.data);
         })
@@ -46,7 +47,8 @@ export default function GetHelp() {
 
      // Then you can update the count property
      numberOfComments[0].count = parseInt(count); // Parse string to integer
-
+        if (search.length > 0 && question.title.rendered.toLowerCase().includes(`${search.toLowerCase()}`)) {
+            console.log(question.title.rendered)
         return (
         <a href="#">
         <div className="card get-help-item mb-4">
@@ -72,6 +74,34 @@ export default function GetHelp() {
         </div>
         </a>
         )
+        } 
+        if (search.length == 0) {
+        return (
+            <a href="#">
+            <div className="card get-help-item mb-4">
+                <div className="card-body">
+                    <div className="row">
+                        <div className='col-lg-3 d-flex align-items-center'>
+                            <div className='get-help'>
+                                <img className="get-help-img mr-3" src={userProfileImg} />
+                                <p><strong>{userName}</strong></p>
+                            </div>
+                        </div>
+                        <div className='col-lg-5 d-flex align-items-center'>
+                            <p>{question.title.rendered}</p>
+                        </div>
+                        <div className='col-lg-2 d-flex align-items-center justify-content-end'>
+                            <p>{days == 0 ? "Posted today" : `${days}d ago`}</p>
+                        </div>
+                        <div className='col-lg-2 d-flex align-items-center justify-content-end'>
+                            <p className="text-right">{numberOfComments[0].count} responses</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </a>
+            )
+        }
     })
 
     return (
@@ -80,8 +110,20 @@ export default function GetHelp() {
             <div className="get-help mb-5">
                 <div className='container primary'>
                     <div className='get-help-details'>
-                        <p><strong>See questions from your peers</strong></p>
-                        <a className="btn btn-info btn-lg">Ask a Question</a>
+                        <div className="row">
+                            <div className="col-lg-4">
+                                <p><strong>See questions from your peers</strong></p>
+                            </div>
+                            <div className="col-lg-4">
+                                <input type="search" className="form-control" placeholder='Start typing to search' value={search} onChange={(e) => {
+                                    setSearch(e.target.value)
+                                    console.log(search)
+                                }} />
+                            </div>
+                            <div className="col-lg-4 text-end">
+                                <a className="btn btn-info btn-lg">Ask a Question</a>
+                            </div>
+                        </div>
                     </div>
                     <hr className="mb-5"></hr>
                     {returnQuestions}
