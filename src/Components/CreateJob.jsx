d47c82d3d9762b287d0b7dc58dc423e347fd9aab
Leshan <leshan0627@gmail.com>
2024-05-13 22:04:02 -0400

    feat: fix spelling error

--- src/Components/CreateJob.jsx
+++ src/Components/CreateJob.jsx
@@ -173,7 +173,7 @@ if (userDetails != null) {
                         </div>
                         <div className="row">
                             <div className="col-12 mb-4">
-                                <input className="form-control form-control-lg" type="text" name="jobs_benefits"  value={createJob.jobs_benefits} onChange={handleChange} aria-label='Job benefits' placeholder="Benefits" autoComplete='on' disabled={ jobStatus === 'publish' ? true : false} required />
+                                <input className="form-control form-control-lg" type="text" name="jobs_benefits"  value={createJob.jobs_benefits} onChange={handleChange} aria-label='Job benefits' placeholder="Benefits and Pay" autoComplete='on' disabled={ jobStatus === 'publish' ? true : false} required />
                             </div>
                         </div>
                         <div className="row">
